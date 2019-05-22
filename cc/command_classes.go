d06809d5286df03dc15942d89ecdefcc47d465ff
Zack Field <zack.field@opendoor.com>
2019-05-22 12:37:49 -0700

    Log additional fields in the error output

--- cc/command_classes.go
+++ cc/command_classes.go
@@ -79,7 +79,7 @@ func Parse(version uint8, payload []byte) (Command, error) {
 	factoriesMu.Unlock()
 
 	if !ok {
-		return nil, fmt.Errorf("%v: %x", ErrNotRegistered, payload[0])
+		return nil, fmt.Errorf("%v: %x %x %x", ErrNotRegistered, payload[0], payload[1], version)
 	}
 
 	command := factory()
