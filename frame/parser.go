234d539869ca0d17b6e2295bda81a0f4df07840b
Isaac Parker <isaac.parker@opendoor.com>
2019-10-15 01:17:46 -0700

    Use constants for FSM events

--- frame/parser.go
+++ frame/parser.go
@@ -9,6 +9,21 @@ import (
 	"go.uber.org/zap"
 )
 
+const (
+	EVENT_PARSE_TIMEOUT    = "PARSE_TIMEOUT"
+	EVENT_RX_SOF           = "RX_SOF"
+	EVENT_RX_ACK           = "RX_ACK"
+	EVENT_RX_CAN           = "RX_CAN"
+	EVENT_RX_NAK           = "RX_NAK"
+	EVENT_RX_LENGTH        = "RX_LENGTH"
+	EVENT_INVALID_LENGTH   = "INVALID_LENGTH"
+	EVENT_RX_DATA          = "RX_DATA"
+	EVENT_RX_DATA_COMPLETE = "RX_DATA_COMPLETE"
+	EVENT_RX_CHECKSUM      = "RX_CHECKSUM"
+	EVENT_CRC_OK           = "CRC_OK"
+	EVENT_CRC_NOTOK        = "CRC_NOTOK"
+)
+
 // ParseStatus respresents an enum for the status parsing a frame.
 type ParseStatus int
 
@@ -63,25 +78,25 @@ func NewParser(ctx context.Context, input <-chan byte, output chan<- *ParseEvent
 	parser.state = fsm.NewFSM(
 		"idle",
 		fsm.Events{
-			{Name: "PARSE_TIMEOUT", Src: []string{"idle", "length", "data", "checksum"}, Dst: "idle"},
-			{Name: "RX_SOF", Src: []string{"idle"}, Dst: "length"},
-			{Name: "RX_ACK", Src: []string{"idle"}, Dst: "idle"},
-			{Name: "RX_CAN", Src: []string{"idle"}, Dst: "idle"},
-			{Name: "RX_NAK", Src: []string{"idle"}, Dst: "idle"},
-			{Name: "RX_LENGTH", Src: []string{"length"}, Dst: "data"},
-			{Name: "INVALID_LENGTH", Src: []string{"length"}, Dst: "idle"},
-			{Name: "RX_DATA", Src: []string{"length", "data"}, Dst: "data"},
-			{Name: "RX_DATA_COMPLETE", Src: []string{"data"}, Dst: "data_complete"},
-			{Name: "RX_CHECKSUM", Src: []string{"data_complete"}, Dst: "checksum"},
-			{Name: "CRC_OK", Src: []string{"checksum"}, Dst: "idle"},
-			{Name: "CRC_NOTOK", Src: []string{"checksum"}, Dst: "idle"},
+			{Name: EVENT_PARSE_TIMEOUT, Src: []string{"idle", "length", "data", "checksum"}, Dst: "idle"},
+			{Name: EVENT_RX_SOF, Src: []string{"idle"}, Dst: "length"},
+			{Name: EVENT_RX_ACK, Src: []string{"idle"}, Dst: "idle"},
+			{Name: EVENT_RX_CAN, Src: []string{"idle"}, Dst: "idle"},
+			{Name: EVENT_RX_NAK, Src: []string{"idle"}, Dst: "idle"},
+			{Name: EVENT_RX_LENGTH, Src: []string{"length"}, Dst: "data"},
+			{Name: EVENT_INVALID_LENGTH, Src: []string{"length"}, Dst: "idle"},
+			{Name: EVENT_RX_DATA, Src: []string{"length", "data"}, Dst: "data"},
+			{Name: EVENT_RX_DATA_COMPLETE, Src: []string{"data"}, Dst: "data_complete"},
+			{Name: EVENT_RX_CHECKSUM, Src: []string{"data_complete"}, Dst: "checksum"},
+			{Name: EVENT_CRC_OK, Src: []string{"checksum"}, Dst: "idle"},
+			{Name: EVENT_CRC_NOTOK, Src: []string{"checksum"}, Dst: "idle"},
 		},
 		fsm.Callbacks{
 			"enter_idle": func(e *fsm.Event) {
 				parser.parseTimeout.Stop()
 				parser.payloadReadBuffer.Reset()
 			},
-			"PARSE_TIMEOUT": func(e *fsm.Event) {
+			EVENT_PARSE_TIMEOUT: func(e *fsm.Event) {
 				event := &ParseEvent{
 					status: ParseTimeout,
 					frame:  Frame{},
@@ -91,32 +106,32 @@ func NewParser(ctx context.Context, input <-chan byte, output chan<- *ParseEvent
 					parser.framesReceived <- event
 				}()
 			},
-			"RX_ACK": func(e *fsm.Event) {
+			EVENT_RX_ACK: func(e *fsm.Event) {
 
 				parser.acks <- true
 			},
-			"RX_NAK": func(e *fsm.Event) {
+			EVENT_RX_NAK: func(e *fsm.Event) {
 				parser.naks <- true
 			},
-			"RX_CAN": func(e *fsm.Event) {
+			EVENT_RX_CAN: func(e *fsm.Event) {
 				parser.cans <- true
 			},
-			"RX_SOF": func(e *fsm.Event) {
+			EVENT_RX_SOF: func(e *fsm.Event) {
 				parser.sof = e.Args[0].(byte)
 				parser.parseTimeout.Reset(readTimeout)
 			},
-			"RX_LENGTH": func(e *fsm.Event) {
+			EVENT_RX_LENGTH: func(e *fsm.Event) {
 				parser.length = e.Args[0].(byte)
 				parser.readCounter = parser.length - 2
 			},
-			"RX_DATA": func(e *fsm.Event) {
+			EVENT_RX_DATA: func(e *fsm.Event) {
 				parser.payloadReadBuffer.WriteByte(e.Args[0].(byte))
 				parser.readCounter--
 			},
 			"checksum": func(e *fsm.Event) {
 				e.Async()
 			},
-			"CRC_OK": func(e *fsm.Event) {
+			EVENT_CRC_OK: func(e *fsm.Event) {
 				parser.l.Debug("event received, attempting to parse")
 
 				event := &ParseEvent{
@@ -130,7 +145,7 @@ func NewParser(ctx context.Context, input <-chan byte, output chan<- *ParseEvent
 					parser.framesReceived <- event
 				}()
 			},
-			"CRC_NOTOK": func(e *fsm.Event) {
+			EVENT_CRC_NOTOK: func(e *fsm.Event) {
 				parser.l.Debug("event received, attempting to parse")
 
 				event := &ParseEvent{
@@ -162,7 +177,7 @@ func (p *Parser) parse() {
 	for {
 		select {
 		case <-p.parseTimeout.C:
-			p.state.Event("PARSE_TIMEOUT")
+			p.state.Event(EVENT_PARSE_TIMEOUT)
 
 		case currentByte := <-p.input:
 			p.processByte(currentByte)
@@ -182,32 +197,32 @@ func (p *Parser) processByte(currentByte byte) {
 	case p.state.Is("idle"):
 		switch currentByte {
 		case HeaderData:
-			p.state.Event("RX_SOF", currentByte)
+			p.state.Event(EVENT_RX_SOF, currentByte)
 		case HeaderAck:
-			p.state.Event("RX_ACK", currentByte)
+			p.state.Event(EVENT_RX_ACK, currentByte)
 		case HeaderCan:
-			p.state.Event("RX_CAN", currentByte)
+			p.state.Event(EVENT_RX_CAN, currentByte)
 		case HeaderNak:
-			p.state.Event("RX_NAK", currentByte)
+			p.state.Event(EVENT_RX_NAK, currentByte)
 		}
 
 	case p.state.Is("length"):
 		if currentByte < minFrameSize || currentByte > maxFrameSize {
-			p.state.Event("INVALID_LENGTH")
+			p.state.Event(EVENT_INVALID_LENGTH)
 		} else {
-			p.state.Event("RX_LENGTH", currentByte)
+			p.state.Event(EVENT_RX_LENGTH, currentByte)
 		}
 
 	case p.state.Is("data"):
 		if p.readCounter > 0 {
-			p.state.Event("RX_DATA", currentByte)
+			p.state.Event(EVENT_RX_DATA, currentByte)
 		} else {
-			p.state.Event("RX_DATA", currentByte)
-			p.state.Event("RX_DATA_COMPLETE")
+			p.state.Event(EVENT_RX_DATA, currentByte)
+			p.state.Event(EVENT_RX_DATA_COMPLETE)
 		}
 
 	case p.state.Is("data_complete"):
-		p.state.Event("RX_CHECKSUM", currentByte)
+		p.state.Event(EVENT_RX_CHECKSUM, currentByte)
 		p.state.Transition()
 
 		payload := p.payloadReadBuffer.Bytes()
@@ -223,10 +238,10 @@ func (p *Parser) processByte(currentByte byte) {
 
 		if frame.VerifyChecksum() == nil {
 			p.l.Debug("checksum verified")
-			p.state.Event("CRC_OK", frame)
+			p.state.Event(EVENT_CRC_OK, frame)
 		} else {
 			p.l.Debug("checksum invalid")
-			p.state.Event("CRC_NOTOK", frame)
+			p.state.Event(EVENT_CRC_NOTOK, frame)
 		}
 
 	}
